c29ac303595446d4253bf3a9d64db8402bc0e953
Utkrishna10 <utkrishna10@gmail.com>
2025-02-03 10:24:47 +0000

    Feb 3, 2025, 10:24 AM

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -3,4 +3,4 @@
 # age = input("What is their age this year? ")
 # personal = input("What would you like to tell them? ")
 # print("Happy",age+"th birthday",name+"!",personal+"!")
-print("I will not sling mud at my " * 500)
+print("I will not sling mud at my friends again" * 500)
