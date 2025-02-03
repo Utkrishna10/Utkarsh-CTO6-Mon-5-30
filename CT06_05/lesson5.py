4333feca104887b7eb727909f91e59056ce8fe66
Utkrishna10 <utkrishna10@gmail.com>
2025-02-03 10:11:21 +0000

    Feb 3, 2025, 10:11 AM

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -1,4 +1,5 @@
 # print("Hello from lesson 5")
 name = input("What is your name? ")
 age = input("What is your age this year? ")
+personal
 print("Happy",age+"th birthday",name+"! Have a happy rest of the year!")
