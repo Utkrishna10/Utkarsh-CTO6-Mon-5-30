efaacd70b06c2dd4b9394d6802652179039cdff8
Utkrishna10 <utkrishna10@gmail.com>
2025-02-03 10:47:08 +0000

    Feb 3, 2025, 10:47 AM

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -6,5 +6,5 @@
 # for count in range(500):
 #     print("I will not sling mud at my friends again.")
 name1 = input("What is your name?")
-for char in name1:
+for abc in name1:
     print("Give me a ",char)
