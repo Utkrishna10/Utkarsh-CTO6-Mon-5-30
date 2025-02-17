1b35c6ab5ded1c6bd5c83611ea37750e74508d55
Utkrishna10 <utkrishna10@gmail.com>
2025-02-17 10:04:30 +0000

    Feb 17, 2025, 10:04 AM

--- CT06_06/lesson6.py
+++ CT06_06/lesson6.py
@@ -4,4 +4,4 @@ numscore = 0
 for i in range(numstudent):
     score = int(input("What is your students score? "))
     numscore = numscore + score
-print(numscore / int(numstudent))
+print("Your classes average score is",numscore / int(numstudent))
