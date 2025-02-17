ce30d935359033b56517130d6f1342391ed1371e
Utkrishna10 <utkrishna10@gmail.com>
2025-02-17 10:01:58 +0000

    Feb 17, 2025, 10:01 AM

--- CT06_06/lesson6.py
+++ CT06_06/lesson6.py
@@ -1,5 +1,5 @@
 # print("Hello from lesson 6")
-numstudent = int(input("How many students do you have?"))
+numstudent = (input("How many students do you have?"))
 numscore = 0
 for i in numstudent:
     score = int(input("What is your students score?"))
