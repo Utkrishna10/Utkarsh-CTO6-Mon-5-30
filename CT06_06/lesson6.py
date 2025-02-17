fc13c245f92ce811944986b5342a99a294240952
Utkrishna10 <utkrishna10@gmail.com>
2025-02-17 10:03:22 +0000

    Feb 17, 2025, 10:03 AM

--- CT06_06/lesson6.py
+++ CT06_06/lesson6.py
@@ -1,7 +1,7 @@
 # print("Hello from lesson 6")
 numstudent = input("How many students do you have? ")
 numscore = 0
-for i in numstudent:
+for i in range(numstudent):
     score = int(input("What is your students score? "))
     numscore = numscore + score
 print(numscore / int(numstudent))
