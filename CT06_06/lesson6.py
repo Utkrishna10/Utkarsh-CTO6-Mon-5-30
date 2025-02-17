8ce171c5219c492aca25a7aeee687060de487563
Utkrishna10 <utkrishna10@gmail.com>
2025-02-17 10:36:06 +0000

    Feb 17, 2025, 10:36 AM

--- CT06_06/lesson6.py
+++ CT06_06/lesson6.py
@@ -1,7 +1,7 @@
 # print("Hello from lesson 6")
 # numstudent = int(input("How many students do you have? "))
 #  numscore = 0
-# # # #  for i in range(numstudent):
+# for i in range(numstudent):
 # # # #      score = int(input("What is your students score? "))
 # # # #      numscore = numscore + score
 # # # #  print("Your classes average score is",numscore / numstudent)
