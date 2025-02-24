81c24388f98f9c28434eb23474b88995064a1eab
Utkrishna10 <utkrishna10@gmail.com>
2025-02-24 10:51:05 +0000

    Feb 24, 2025, 10:51 AM

--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -22,9 +22,9 @@
 #     print("Nice to meet you",name)
 for i in range(5):
     no1 = int(input("What is your number 1 "))
-# no2 = int(input("What is your number 2 "))
-# no3 = int(input("What is your number 3 "))
-# no4 = int(input("What is your number 4 "))
+    no2 = int(input("What is your number 2 "))
+    no3 = int(input("What is your number 3 "))
+    no4 = int(input("What is your number 4 "))
 # no5 = int(input("What is your number 5 "))
 # total = no1 + no2 + no3 + no4 + no5
 # print("The sum of your numbers are",total)
