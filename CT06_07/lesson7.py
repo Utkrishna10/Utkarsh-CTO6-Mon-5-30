72d8ef16fa5027a8500ab6a60a4fc57b61840eca
Utkrishna10 <utkrishna10@gmail.com>
2025-02-24 10:51:59 +0000

    Feb 24, 2025, 10:51 AM

--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -20,8 +20,10 @@
 # no = int(input("How many times would to like me to print it "))
 # for i in range(no):
 #     print("Nice to meet you",name)
+total = 0
 for i in range(5):
     i = int(input("What is your number",i))
+    
 # total = no1 + no2 + no3 + no4 + no5
 # print("The sum of your numbers are",total)
 # time = int(input("What is your number "))
