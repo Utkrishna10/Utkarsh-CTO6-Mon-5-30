b1651007e49604125d5d0c4e3d71d9f6b5c9b3df
Utkrishna10 <utkrishna10@gmail.com>
2025-02-24 10:48:37 +0000

    Feb 24, 2025, 10:48 AM

--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -33,7 +33,7 @@
 # no = int(input("What is your number "))
 # for i in range(no + 1):
 #     print(str(i) * i)
-for count in range(5):
+for i in range(5):
     count = int(input("What is your number "))
 total = 1 + 2 + 3 + 4 + 5 
 print("Your students average score is",int(total)/5)
