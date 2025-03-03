cac7ce70448da411dbc408b34cd42a7f22de0b33
Utkrishna10 <utkrishna10@gmail.com>
2025-03-03 10:14:46 +0000

    Mar 3, 2025, 10:14 AM

--- CT06_07/Midsem.py
+++ CT06_07/Midsem.py
@@ -2,8 +2,8 @@
 #name = input("What is your name? ")
 #print("Nice to meet you,", name+"!")
 # q2
-start = int(input("What number would you like to start at? "))
-end = int(input("What number would you like to end at? "))
-increment = int(input("What number would you like to skip by? "))
-for number in range(start, end, increment):
-    print(number)
+# start = int(input("What number would you like to start at? "))
+# end = int(input("What number would you like to end at? "))
+# increment = int(input("What number would you like to skip by? "))
+# for number in range(start, end, increment):
+#     print(number)
