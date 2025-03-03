a73c2e2cba8c94ec76a5e46ffb1bb52b2e442887
Utkrishna10 <utkrishna10@gmail.com>
2025-03-03 10:57:57 +0000

    Mar 3, 2025, 10:57 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -1,7 +1,7 @@
 print("Hello from lesson 8")
 product = 1
 for count in range(5):
-    num = input("Give me a number")
+    num = input("Give me a number ")
     num = int(num)
     product = product * num
 print("The final result is", str(product))
