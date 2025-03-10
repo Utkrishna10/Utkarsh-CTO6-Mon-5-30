fc61231d7c6d38209ba28f413a5bcc38b16ac9ae
Utkrishna10 <utkrishna10@gmail.com>
2025-03-10 10:51:50 +0000

    Mar 10, 2025, 10:51 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -23,7 +23,7 @@ import random
 num1 = random.randint(1,100)
 num2 = random.randint(1,100)
 test = "What is", num1,"+",num2,"?"
-cor = input(test )
+cor = input(test)
 cor = int(cor)
 hidden = int(num1) + int(num2)
 print(cor == hidden)
