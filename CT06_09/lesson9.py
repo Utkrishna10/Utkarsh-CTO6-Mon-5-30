6ca6edba28deeb6dbf7604ce6f2f5a1b8db70d2e
Utkrishna10 <utkrishna10@gmail.com>
2025-03-17 10:50:50 +0000

    Mar 17, 2025, 10:50 AM

--- CT06_09/lesson9.py
+++ CT06_09/lesson9.py
@@ -1,4 +1,4 @@
 # print("Hello from lesson 9")
 import random
 num = random.randint(1,10)
-num1 = int(input("What number from 1 "))
+num1 = int(input("What number from 1 to 10 do you think it is? "))
