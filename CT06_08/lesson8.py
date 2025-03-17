eb97c48122fe3f48485c207b4b9b0c15a4464dcd
Utkrishna10 <utkrishna10@gmail.com>
2025-03-17 09:43:59 +0000

    Mar 17, 2025, 9:43 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -20,16 +20,16 @@ import random
 # var = True
 # varss = False
 # print(var == varss)
-num1 = random.randint(1,100)
-num2 = random.randint(1,100)
-test = "What is", num1,"+",num2,"?"
-cor = input(test)
-cor = int(cor)
-hidden = int(num1) + int(num2)
-if cor == hidden:
-    print("Correct!")
-else:
-    print("Wrong")
+# num1 = random.randint(1,100)
+# num2 = random.randint(1,100)
+# test = "What is", num1,"+",num2,"?"
+# cor = input(test)
+# cor = int(cor)
+# hidden = int(num1) + int(num2)
+# if cor == hidden:
+#     print("Correct!")
+# else:
+#     print("Wrong")
 # print(cor == hidden)
 num = random.randint(1,50)
 corr1 = input("What number would you like to start at? ")
