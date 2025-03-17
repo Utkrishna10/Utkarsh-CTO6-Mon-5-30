49561c46c7dffd41d5a4a018a1f73f73e7250a0a
Utkrishna10 <utkrishna10@gmail.com>
2025-03-17 10:53:42 +0000

    Mar 17, 2025, 10:53 AM

--- CT06_09/lesson9.py
+++ CT06_09/lesson9.py
@@ -3,6 +3,6 @@ import random
 num = random.randint(1,10)
 num1 = int(input("What number from 1 to 10 do you think it is? "))
 if num == num1:
-    print("Congradulations! You were correct!")
+    print("Congratulations! You were correct!")
 else:
     print("Sorry. You were wrong. The correct answer was",num)
