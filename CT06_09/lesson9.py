d66e2b44e7066031ebd2c2b360ee0af74d3fdb03
Utkrishna10 <utkrishna10@gmail.com>
2025-03-24 09:57:06 +0000

    Mar 24, 2025, 9:57 AM

--- CT06_09/lesson9.py
+++ CT06_09/lesson9.py
@@ -9,10 +9,10 @@
 apple = int(input("How many apples would you like to buy? "))
 orange = int(input("How many oranges would you like to buy? "))
 apple = apple * 0.60
+orange = orange * 0.90
 if apple >= 5:
     apple = apple/10 * 90
 if orange >= 5:
-    orange = orange * 0.90
     orange = orange/10 * 90
 total = apple + orange
 print("The total price of your items are", total)
