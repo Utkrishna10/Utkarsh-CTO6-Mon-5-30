2cd7c296a7124c96306efbc2fd8bdd3c725c6b38
Utkrishna10 <utkrishna10@gmail.com>
2025-03-24 09:41:05 +0000

    Mar 24, 2025, 9:41 AM

--- CT06_09/lesson9.py
+++ CT06_09/lesson9.py
@@ -1,8 +1,8 @@
 # print("Hello from lesson 9")
-import random
-num = random.randint(1,10)
-num1 = int(input("What number from 1 to 10 do you think it is? "))
-if num == num1:
-    print("Congratulations! You were correct!")
-else:
-    print("Sorry. You were wrong. The correct answer was",num)
+# import random
+# num = random.randint(1,10)
+# num1 = int(input("What number from 1 to 10 do you think it is? "))
+# if num == num1:
+#     print("Congratulations! You were correct!")
+# else:
+#     print("Sorry. You were wrong. The correct answer was",num)
