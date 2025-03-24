bb6fd95d3a82867982e61ebb2391625639ea65d9
Utkrishna10 <utkrishna10@gmail.com>
2025-03-24 10:34:09 +0000

    Mar 24, 2025, 10:34 AM

--- CT06_09/lesson9.py
+++ CT06_09/lesson9.py
@@ -22,8 +22,8 @@ import random
 #     if daytemp >= 30:
 #         hotday = hotday + 1
 # print("The amount of days that you recorded a tempreture over 30 degrees in this week is", str(hotday)+". ")
-num = int(input("What number would you like to check? "))
-if num >= 0:
-    print("Your number is a positive number! ")
-else:
-    print("Your number is a negative number! ")
+# num = int(input("What number would you like to check? "))
+# if num >= 0:
+#     print("Your number is a positive number! ")
+# else:
+#     print("Your number is a negative number! ")
