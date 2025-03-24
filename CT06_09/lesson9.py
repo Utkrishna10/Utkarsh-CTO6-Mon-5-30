699511fe7ee3bd17b587b6a3d0ec71dcce6eb849
Utkrishna10 <utkrishna10@gmail.com>
2025-03-24 10:34:19 +0000

    Mar 24, 2025, 10:34 AM

--- CT06_09/lesson9.py
+++ CT06_09/lesson9.py
@@ -27,3 +27,9 @@ import random
 #     print("Your number is a positive number! ")
 # else:
 #     print("Your number is a negative number! ")
+num = random.randint(1,10)
+num1 = int(input("What number from 1 to 10 do you think it is? "))
+# if num == num1:
+#     print("Congratulations! You were correct!")
+# else:
+#     print("Sorry. You were wrong. The correct answer was",num)
