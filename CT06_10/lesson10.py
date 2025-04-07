a7ee146fed1bf5c3aedd1067241582547f60477a
Utkrishna10 <utkrishna10@gmail.com>
2025-04-07 10:10:22 +0000

    Apr 7, 2025, 10:10 AM

--- CT06_10/lesson10.py
+++ CT06_10/lesson10.py
@@ -10,10 +10,10 @@
 #     print("Your grade is D!")
 # else:
 #     print("Your grade is F!")
-age = int(input("What is your age? "))
-if age <= 0:
-    print("Your age cannot be negative!")
-elif age < 18:
-    print("You are not eligible to vote!")
-else:
-    print("You are eligible to vote!")
+# age = int(input("What is your age? "))
+# if age <= 0:
+#     print("Your age cannot be negative!")
+# elif age < 18:
+#     print("You are not eligible to vote!")
+# else:
+#     print("You are eligible to vote!")
