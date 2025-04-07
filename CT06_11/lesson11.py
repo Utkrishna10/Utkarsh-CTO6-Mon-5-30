d07f7e80161648bd8dfc2633c28ae338a354e982
Utkrishna10 <utkrishna10@gmail.com>
2025-04-07 10:24:45 +0000

    Apr 7, 2025, 10:24 AM

--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -1,8 +1,8 @@
 # print("Hello from lesson 11")
-rider1 = 125
-rider2 = 150
-if rider1 >= 120 and rider2 >= 120:
-    print("You can ride the ride!")
-else:
-    print("You cannot ride the ride.")
-px = int(input("What is the pr"))
+# rider1 = 125
+# rider2 = 150
+# if rider1 >= 120 and rider2 >= 120:
+#     print("You can ride the ride!")
+# else:
+#     print("You cannot ride the ride.")
+px = int(input("What is the price of the item?"))
