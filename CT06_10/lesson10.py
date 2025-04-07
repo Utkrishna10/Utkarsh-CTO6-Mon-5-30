97d5b1e37cdd6c8e8522b1769c7fcb63d594734b
Utkrishna10 <utkrishna10@gmail.com>
2025-04-07 09:47:53 +0000

    Apr 7, 2025, 9:47 AM

--- CT06_10/lesson10.py
+++ CT06_10/lesson10.py
@@ -1,4 +1,7 @@
 # print("Hello from lesson 10")
 grade = int(input("What is your score?"))
-if grade > 89:
+if grade >= 90:
     print("Your grade is A!")
+elif grade >= 80:
+    print("Your grade is B!")
+    
