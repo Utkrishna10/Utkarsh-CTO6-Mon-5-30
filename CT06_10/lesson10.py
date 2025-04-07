872f6d9a7afb70542063629354fbb0e1cba3a6b8
Utkrishna10 <utkrishna10@gmail.com>
2025-04-07 09:52:31 +0000

    Apr 7, 2025, 9:52 AM

--- CT06_10/lesson10.py
+++ CT06_10/lesson10.py
@@ -1,12 +1,13 @@
 # print("Hello from lesson 10")
-grade = int(input("What is your score? "))
-if grade >= 90:
-    print("Your grade is A!")
-elif grade >= 80:
-    print("Your grade is B!")
-elif grade >= 70:
-    print("Your grade is C!")
-elif grade >= 60:
-    print("Your grade is D!")
-else:
-    print("Your grade is F!")
+# grade = int(input("What is your score? "))
+# if grade >= 90:
+#     print("Your grade is A!")
+# elif grade >= 80:
+#     print("Your grade is B!")
+# elif grade >= 70:
+#     print("Your grade is C!")
+# elif grade >= 60:
+#     print("Your grade is D!")
+# else:
+#     print("Your grade is F!")
+age = int()
