0087785c5bf662da3b99186b5221e59b25dd254f
Utkrishna10 <utkrishna10@gmail.com>
2025-04-07 09:54:41 +0000

    Apr 7, 2025, 9:54 AM

--- CT06_10/lesson10.py
+++ CT06_10/lesson10.py
@@ -14,4 +14,6 @@ age = int(input("What is your age? "))
 if age <= 0:
     print("Your age cannot be negative!")
 elif age < 18:
-    print("You are not elidgable")
+    print("You are not elidgable to vote!")
+else:
+    
