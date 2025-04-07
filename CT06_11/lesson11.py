c63159f7dc8e8e7043edbf79410364d7785b819c
Utkrishna10 <utkrishna10@gmail.com>
2025-04-07 10:23:59 +0000

    Apr 7, 2025, 10:23 AM

--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -1,7 +1,7 @@
 # print("Hello from lesson 11")
 rider1 = 125
 rider2 = 150
-if rider1 and rider2 >= 120:
+if rider1 >= 120 and rider2 >= 120:
     print("You can ride the ride!")
 else:
     print("You cannot ride the ride.")
