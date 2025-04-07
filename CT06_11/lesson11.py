5e24755d3126483588d6a4917cb340d4928010f7
Utkrishna10 <utkrishna10@gmail.com>
2025-04-07 10:23:22 +0000

    Apr 7, 2025, 10:23 AM

--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -1,8 +1,8 @@
 # print("Hello from lesson 11")
-# rider1 = 125
-# rider2 = 150
-# if rider1 and rider2 >= 120:
-#     print("You can ride the ride!")
-# else:
-#     print("You cannot ride the ride.")
+rider1 = 125
+rider2 = 150
+if rider1 and rider2 >= 120:
+    print("You can ride the ride!")
+else:
+    print("You cannot ride the ride.")
 px = int()
